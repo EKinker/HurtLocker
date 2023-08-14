3535641de122ba6047354221e08a3c909d8d34fc
Liz Kinker <Liz.Kinker@gmail.com>
2023-08-14 10:37:42 -0400

    Split list into an array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -13,5 +16,15 @@ public class Main {
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
 
+        System.out.println(splitIntoTokens(output).size());
+
+    }
+
+    public static List<String> splitIntoTokens(String input){
+        List<String> tokens = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(input, "##");
+        while (st.hasMoreTokens()){
+        tokens.add(st.nextToken());}
+       return tokens;
     }
 }
